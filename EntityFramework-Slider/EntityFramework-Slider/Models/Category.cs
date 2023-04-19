67aad6f5922c4eb24ff6442c0e5ca2b1219aa1d1
unknown <royaam@code.edu.az>
2023-04-20 00:53:37 +0400

    task are ready

---
+++ EntityFramework-Slider/EntityFramework-Slider/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework_Slider.Models
+{
+    public class Category:BaseEntity
+    {
+        [Required]  //Name null ola bilmez
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }   /*Categoriden  yola cixanda Producta catmaq ucun*/
+    }
+}
